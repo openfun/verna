ebd050b10a0d55580525c87c309d5631a6435af9
Theo <theo.clement@polyconseil.fr>
2022-05-11 16:47:05 +0200

    ✅(jest) add a custom resolver to enforce the use of common js
    Since jest 28, module import logic has changed and packages which have
    es module exports could encounter problem with jest-environment-jsdom as jest
    uses the es module exports instead of the common js. To address this issue we
    have to use a custom resolver, to force the use of the common js for
    problematic packages (nanoid).
    
    https://jestjs.io/docs/next/upgrading-to-jest28#packagejson-exports

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,9 @@
-const path = require('path');
-
 module.exports = {
   coverageDirectory: '.coverage',
-  moduleDirectories: [path.resolve(__dirname, 'src'), 'node_modules'],
+  moduleDirectories: ['<rootDir>/src', 'node_modules'],
   moduleFileExtensions: ['js', 'ts', 'tsx'],
-  setupFilesAfterEnv: [path.resolve(__dirname, 'jest.setup.js')],
+  resolver: '<rootDir>/jest/resolver.js',
+  setupFilesAfterEnv: ['<rootDir>/jest/setup.js'],
   testEnvironment: 'jsdom',
-  testMatch: [path.resolve(__dirname, 'src/**/*.spec.+(ts|tsx)')],
-  transformIgnorePatterns: ['node_modules/(?!(nanoid)/)'],
+  testMatch: ['<rootDir>/src/**/*.spec.+(ts|tsx)'],
 };
