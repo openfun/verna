ba2d3209e469083135a643fa1dd47580845741c5
jbpenrath <jb.penrath@gmail.com>
2022-07-06 21:58:32 +0200

    🔧(lib) update build configuration
    With the move to monorepo, some ts error appears. We have to update the build
    configuration to fix that.

--- examples/playground/vite.config.ts
+++ examples/playground/vite.config.ts
@@ -4,4 +4,15 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: [
+      {
+        // We want to benefit of hmr when we are developing on @openfun/verna
+        // workspace. A little hack to achieve this it to use an alias to resolve
+        // the module through src instead of dist.
+        find: /^@openfun\/verna$/,
+        replacement: '@openfun/verna/src/index.ts',
+      },
+    ],
+  },
 });
--- lib/vite.config.ts
+++ lib/vite.config.ts
@@ -7,23 +7,31 @@ import typescript from '@rollup/plugin-typescript';
 export default defineConfig({
   build: {
     lib: {
-      entry: resolve(__dirname, 'src/index.ts'),
-      fileName: (format) => `verna.${format}.js`,
+      entry: resolve(__dirname, './src/index.ts'),
+      fileName: (format) => (format === 'es' ? 'index.js' : `index.${format}.js`),
+      formats: ['es', 'umd'],
       name: 'index',
     },
     rollupOptions: {
-      external: ['react'],
+      external: ['react', 'react-dom', 'react-intl'],
       output: {
         globals: {
           react: 'React',
+          'react-dom': 'ReactDOM',
+          'react-intl': 'ReactIntl',
         },
       },
       plugins: [
         typescript({
-          allowSyntheticDefaultImports: true,
           declaration: true,
-          declarationDir: resolve(__dirname, './dist/types'),
-          exclude: resolve(__dirname, './node_modules/**'),
+          declarationDir: resolve(__dirname, './dist'),
+          exclude: [
+            resolve(__dirname, './dist'),
+            resolve(__dirname, './node_modules/**'),
+            // Ignore all test stuff
+            resolve(__dirname, './src/tests'),
+            resolve(__dirname, './src/**/*.spec.tsx?'),
+          ],
           noEmitOnError: true,
           rootDir: resolve(__dirname, './src'),
           target: 'ESNext',
