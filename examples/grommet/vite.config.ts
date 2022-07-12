acc88aa59d091560e42c9c628771e4f40768dcea
Theo <theo.clement@polyconseil.fr>
2022-07-12 10:19:31 +0200

    notworkingrebase

--- examples/grommet/vite.config.ts
+++ examples/grommet/vite.config.ts
@@ -1,13 +1,18 @@
 import { defineConfig } from 'vite';
-import { resolve } from 'path';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
-    alias: {
-      '@openfun/verna': resolve(__dirname, '../../src/'),
-    },
+    alias: [
+      {
+        // We want to benefit of hmr when we are developing on @openfun/verna
+        // workspace. A little hack to achieve this it to use an alias to resolve
+        // the module through src instead of dist.
+        find: /^@openfun\/verna$/,
+        replacement: '@openfun/verna/src/index.ts',
+      },
+    ],
   },
 });
