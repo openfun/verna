122eebe9d12dd6c283f57ebe7dbcd51510afc6fb
jbpenrath <jb.penrath@gmail.com>
2022-12-07 09:24:38 +0100

    🔧(lib) remove use of @rollup/typescript into vite dev conf
    TMP

--- lib/vite.dev.config.ts
+++ lib/vite.dev.config.ts
@@ -1,7 +1,7 @@
 import { resolve } from 'path';
-import typescript from '@rollup/plugin-typescript';
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
+import dts from 'vite-plugin-dts';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -24,26 +24,13 @@ export default defineConfig({
       include: [resolve(__dirname, './src/**')],
     },
   },
-  plugins: [
-    react(),
-    typescript({
-      declaration: true,
-      declarationDir: resolve(__dirname, './dist'),
-      exclude: [
-        resolve(__dirname, './dist'),
-        resolve(__dirname, './node_modules/**'),
-        // Ignore all test stuff
-        resolve(__dirname, './src/tests'),
-        resolve(__dirname, './src/**/*.spec.tsx?'),
-      ],
-      noEmitOnError: false,
-      rootDir: resolve(__dirname, './src'),
-      target: 'ESNext',
-    }),
-  ],
+  plugins: [dts(), react()],
   resolve: {
-    alias: {
-      ':/': resolve(__dirname, './src'),
-    },
+    alias: [
+      {
+        find: ':',
+        replacement: resolve(__dirname, './src'),
+      },
+    ],
   },
 });
